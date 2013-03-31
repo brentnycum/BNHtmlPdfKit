125f9c12b32effcad329f48c1b9dc721c27c6633
Brent Nycum <brentnycum@gmail.com>
2013-03-30 23:12:09 -0500

    Creating BNHtmlPrinter.

---
+++ BNHtmlPrinter.h
@@ -0,0 +1,12 @@
+//
+//  BNHtmlPrinter.h
+//
+//  Created by Brent Nycum.
+//  Copyright (c) 2013 Brent Nycum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNHtmlPrinter : NSObject
+
+@end;
---
+++ BNHtmlPrinter.m
@@ -0,0 +1,13 @@
+//
+//  BNHtmlPrinter.m
+//
+//  Created by Brent Nycum.
+//  Copyright (c) 2013 Brent Nycum. All rights reserved.
+//
+
+#import "BNHtmlPrinter.h"
+
+@implementation BNHtmlPrinter
+
+@end
+
