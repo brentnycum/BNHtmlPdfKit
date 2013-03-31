2e29b892660a674eb325ea70d0af9037e4aac34d
Brent Nycum <brentnycum@gmail.com>
2013-03-30 23:19:22 -0500

    Creating BNPageSize enum.

--- BNHtmlPrinter.h
+++ BNHtmlPrinter.h
@@ -7,6 +7,63 @@
 
 #import <Foundation/Foundation.h>
 
+// http://en.wikipedia.org/wiki/Paper_size
+typedef enum {
+	BNPageSizeLetter,
+	BNPageSizeGovernmentLetter,
+	BNPageSizeLegal,
+	BNPageSizeJuniorLegal,
+	BNPageSizeLedger,
+	BNPageSizeTabloid,
+	BNPageSizeA0,
+	BNPageSizeA1,
+	BNPageSizeA2,
+	BNPageSizeA3,
+	BNPageSizeA4,
+	BNPageSizeA5,
+	BNPageSizeA6,
+	BNPageSizeA7,
+	BNPageSizeA8,
+	BNPageSizeA9,
+	BNPageSizeA10,
+	BNPageSizeB0,
+	BNPageSizeB1,
+	BNPageSizeB2,
+	BNPageSizeB3,
+	BNPageSizeB4,
+	BNPageSizeB5,
+	BNPageSizeB6,
+	BNPageSizeB7,
+	BNPageSizeB8,
+	BNPageSizeB9,
+	BNPageSizeB10,
+	BNPageSizeC0,
+	BNPageSizeC1,
+	BNPageSizeC2,
+	BNPageSizeC3,
+	BNPageSizeC4,
+	BNPageSizeC5,
+	BNPageSizeC6,
+	BNPageSizeC7,
+	BNPageSizeC8,
+	BNPageSizeC9,
+	BNPageSizeC10,
+	BNPageSizeJapaneseB0,
+	BNPageSizeJapaneseB1,
+	BNPageSizeJapaneseB2,
+	BNPageSizeJapaneseB3,
+	BNPageSizeJapaneseB4,
+	BNPageSizeJapaneseB5,
+	BNPageSizeJapaneseB6,
+	BNPageSizeJapaneseB7,
+	BNPageSizeJapaneseB8,
+	BNPageSizeJapaneseB9,
+	BNPageSizeJapaneseB10,
+	BNPageSizeJapaneseB11,
+	BNPageSizeJapaneseB12,
+	BNPageSizeCustom
+} BNPageSize;
+
 @interface BNHtmlPrinter : NSObject
 
 @end;
